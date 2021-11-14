1d3ba84f674a78248ee91f71dbf7843f6952c8c7
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2021-11-14 15:51:52 -0500

    Used a for loop to repeat the song chorus

---
+++ chorus-for.js
@@ -0,0 +1,10 @@
+/* Looping & Repetition Exercise */
+
+/* For loops */ 
+
+// Example 1: prints "lets dance" 11 times, then prints "until the sun comes up"
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
