e6b468b5695d2cb48450ce58f49c71f5f4431839
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2021-11-15 00:05:51 -0500

    Added some tests for console log and return

--- hello.js
+++ hello.js
@@ -1,8 +1,23 @@
 /* Declaring and Calling Functions Assignment */
 
+// Exercise 1: sayHello Function 
 const sayHello  = function (name) {
   console.log("Hello " + name);
 }
 
 sayHello("Rebecca");
 sayHello("Bob");
+
+// console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+// return
+const returnSayHello  = function (name) {
+  return "Hi " + name;
+}
+const greeting = returnSayHello('Marry');
+
+console.log(greeting);
