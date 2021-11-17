7f04d7d83cc314ac4043e933c5f330af2058e710
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2021-11-17 03:46:20 -0500

    Used for loop to print array list

---
+++ packing-list.js
@@ -0,0 +1,13 @@
+// Iterating Over Arrays exercise
+
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+// accessing an array item
+packingList[2];
+
+// using for loop to print the entire list
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
