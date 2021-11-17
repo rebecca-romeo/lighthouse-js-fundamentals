f787eb3e6415ca44be91bd91a21f239fb7f29d8d
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2021-11-17 03:49:24 -0500

    Used while loop  to print the list

--- packing-list.js
+++ packing-list.js
@@ -11,3 +11,11 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// using while loop to print the list 
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
